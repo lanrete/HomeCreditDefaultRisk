a21bd640f8b8a9614720c07ddacc056b3b01d502
lanrete <jeorito@gmail.com>
2018-06-27 01:04:56 +0800

    3 scripts were added in this commit --- 1) ./code/pre_application_data_prepare.py ------ This file is currently empty, serve as a ------ placeholder for future data preparation ------ on previous_application.csv --- 2) ./notebook/EAD on previous application.ipynb ------ As named, this notebook contains some ------ basic EAD on previous_application.csv --- 3) ./notebook/previous_application_science_view_trial.py ------ This is the first trial of the PyCharm's ------ SciView functioanlity in Professional Edition ------ This script also hold some other EAD on the ------ previous application. The on-going EAD-related ------ work will be performed in similar fashion. ------ While the final share-able model kernel will be ------ in another saparate jupyter notebook once the ------ competition is over. ------ SO BASICALLY, SciView for development/exploration ------               Jupyter for sharing/showing/documentation

---
+++ code/pre_application_data_prepare.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3  
+# -*- coding: utf-8 -*-  
+"""  
+Created by Lanrete on 2018/6/23
+
+To summarize the information in previous_application.csv
+Aggregate into SK_ID_CURR level
+"""
+
+
+# TODO  Understand the data structure and generate features
